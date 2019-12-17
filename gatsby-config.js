4ca4a8eef482fae98709003c3d768db6a1f28dcf
mjaydenkim <mjaydenkim@gmail.com>
2019-12-17 11:02:26 -0800

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,11 @@
 let siteMetadata = {
-    title: `Elemental`,
+    title: `UBI`,
     capitalizeTitleOnHome: true,
     logo: `/images/logo.png`,
     icon: `/images/icon.png`,
     titleImage: `/images/wall.jpg`,
-    introTag: `PHOTOGRAPHER | VIDEOGRAPHER`,
-    description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet accumsan arcu. Proin ac consequat arcu.`,
+    introTag: `English Position Project for Musich P7`,
+    description: `Is a Universal Basic Income of $1000 per month a good way to solve the problem of job displacement by automation?`,
     author: `@_akzhy`,
     blogItemsPerPage: 10,
     portfolioItemsPerPage: 10,
@@ -67,11 +67,11 @@ let siteMetadata = {
     ],
     contact: {
         /* Leave the below value completely empty (no space either) if you don't want a contact form. */
-        api_url: "./test.json",
+        api_url: "",
         description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet accumsan arcu. Proin ac consequat arcu.`,
-        mail: "hi@akzhy.com",
-        phone: "000-000-0000",
-        address: "1234 \nLocation \nLocation"
+        mail: "mjaydenkim@gmail.com",
+        phone: "N/A",
+        address: "N/A"
     }
 };
 

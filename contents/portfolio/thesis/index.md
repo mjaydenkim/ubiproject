c85a9eb62c6a1b5596208458b3cdcfb371c7c67d
Matthew Kim <mjaydenkim@gmail.com>
2019-12-17 20:28:07 -0800

    added stuff

---
+++ contents/portfolio/thesis/index.md
@@ -0,0 +1,8 @@
+---
+title: Thesis- Why is a UBI a good idea?
+description: Why is a Universal Basic Income a good way to solve the problem of job displacement by automation?
+date: 2019-12-17
+template: portfolio
+image: ./image.jpg
+---
+
---
+++ gatsby-config.js
@@ -0,0 +1,112 @@
+let siteMetadata = {
+    title: `Universal Basic Income`,
+    capitalizeTitleOnHome: true,
+    logo: `/images/logo.png`,
+    icon: `/images/icon.png`,
+    titleImage: `/images/wall.jpg`,
+    introTag: `English Position Project for Musich, P7`,
+    description: `Is a Universal Basic Income of $1000 per month a good way to solve the problem of job displacement by automation?`,
+    author: `@_akzhy`,
+    blogItemsPerPage: 10,
+    portfolioItemsPerPage: 10,
+    darkmode: true,
+    switchTheme: true,
+    navLinks: [
+        {
+            name: "HOME",
+            url: "/"
+        },
+        {
+            name: "ABOUT",
+            url: "/about"
+        },
+        {
+            name: "BLOG",
+            url: "/blog"
+        },
+        {
+            name: "PORTFOLIO",
+            url: "/portfolio"
+        },
+        {
+            name: "CONTACT",
+            url: "/contact"
+        }
+    ],
+    footerLinks: [
+        {
+            name: "PRIVACY POLICY",
+            url: "/privacy-policy"
+        },
+        {
+            name: "GitHub",
+            url: "https://github.com/akzhy/gatsby-starter-elemental"
+        }
+    ],
+    social: [
+        {
+            name: "Facebook",
+            icon: "/images/Facebook.svg",
+            url: "#"
+        },
+        {
+            name: "Twitter",
+            icon: "/images/Twitter.svg",
+            url: "#"
+        },
+        {
+            name: "Instagram",
+            icon: "/images/Instagram.svg",
+            url: "#"
+        },
+        {
+            name: "Youtube",
+            icon: "/images/Youtube.svg",
+            url: "#"
+        }
+    ],
+    contact: {
+        /* Leave the below value completely empty (no space either) if you don't want a contact form. */
+        api_url: "",
+        description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet accumsan arcu. Proin ac consequat arcu.`,
+        mail: "mjaydenkim@gmail.com",
+        phone: "N/A",
+        address: "N/A"
+    }
+};
+
+module.exports = {
+    siteMetadata: siteMetadata,
+    plugins: [
+        `gatsby-plugin-sharp`,
+        `gatsby-transformer-sharp`,
+        `gatsby-plugin-react-helmet`,
+        {
+            resolve: `gatsby-transformer-remark`,
+            options: {
+                plugins: [
+                    "gatsby-remark-copy-linked-files",
+                    {
+                        resolve: `gatsby-remark-images`,
+                        options: {
+                            maxWidth: 1280
+                        }
+                    }
+                ]
+            }
+        },
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                name: `contents`,
+                path: `${__dirname}/contents/`
+            }
+        },
+        {
+            resolve: `gatsby-plugin-less`,
+            options: {
+                strictMath: true
+            }
+        }
+    ]
+};
